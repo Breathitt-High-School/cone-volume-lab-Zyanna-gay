7a3bd8212ae842ff1cbbc6e2934d1e4af6a1a48b
JoyNeace <7303265+JoyNeace@users.noreply.github.com>
2020-10-01 13:28:42 -0400

    Initial commit

---
+++ ConeRunner.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class ConeRunner
+{
+	public static void main( String[] args )
+	{
+		Scanner kb = new Scanner( System.in );
+		System.out.print( "Enter the radius :: " );
+		int radius = kb.nextInt();
+		System.out.print( "Enter the height :: " );		
+		int height = kb.nextInt();
+		
+		//print your answer here
+	}
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Cone-Volume-Lab
+## Lab Goal :   This lab was designed to teach value calculation and output.
+
+## Lab Description :   Write a program to calculate the volume of a cone.  
+
+Use the following formula.
+
+volume = 1/3 π r2 h    
+
+r is the radius and h is the height
